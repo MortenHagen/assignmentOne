96d72b8aedd712b0e3061095727a9189b49981e6
MortenHagen <mortenhagen30@gmail.com>
2024-02-18 14:15:34 +0100

    added mainpge and starterlogic

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const menuItems = document.querySelectorAll(".main-menu li");
+    const images = document.querySelectorAll(".picture-container img");
+
+    menuItems.forEach(item => {
+        item.addEventListener("mouseover", function() {
+            const dataImg = this.getAttribute("data-menu");
+            images.forEach(img => {
+                if (img.getAttribute("data-img") === dataImg) {
+                    img.style.display = "block";
+                } else {
+                    img.style.display = "none";
+                }
+            });
+        });
+    });
+});
